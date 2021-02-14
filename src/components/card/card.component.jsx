6521158c2f04fe5a1db8e2eb7ebd1922184aa775
Thomas Vercamer <thomas.vercamer@gmail.com>
2021-02-14 15:55:49 +0100

    Add Searchbox component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './card.styles.css';
 
-export const Card = (props) => (
+export const Card = ({monster}) => (
     <div className='card-container'>
-        <img src={`https://robohash.org/${props.id}?set=set2&size=180x180`} alt="monster"/>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt="monster"/>
+        <h2>{monster.name}</h2>
+        <p>{monster.email}</p>
     </div>
 )
