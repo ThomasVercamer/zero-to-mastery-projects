d06ce988008de6dc0caec6bb891662e661ba088a
Thomas Vercamer <thomas.vercamer@gmail.com>
2021-02-17 14:08:33 +0100

    Refactor into smaller components + add menu styling

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './directory.styles.scss';
+import sections from './directory.data';
+import MenuItem from '../menu-item/menu-item.component';
+
+class Directory extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sections
+        };
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {this.state.sections.map(({id, title, imageUrl, size}) => <MenuItem
+                key={id}
+                title={title}
+                imageUrl={imageUrl}
+                size={size} />)}
+            </div>
+        )
+    }
+}
+
+export default Directory;
