d6d8302663e38e57e2d139dffa862c40bf6557bd
Thomas Vercamer <thomas.vercamer@gmail.com>
2021-02-23 16:44:51 +0100

    Refactor with reselect and memoization

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import {connect} from 'react-redux';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
@@ -13,7 +14,7 @@ const CartDropdown = ({cartItems}) => (
 );
 
 const mapStateToProps = state => ({
-    cartItems: state.cart.cartItems
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
