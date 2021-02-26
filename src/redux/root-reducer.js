46f09af173af34eb719eea05d47a02cdf44a5a8a
Thomas Vercamer <thomas.vercamer@gmail.com>
2021-02-26 08:42:39 +0100

    Persist cart items in session storage using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root', // start from the root reducer
+    storage: storageSession,
+    whitelist: ['cart'] // names of the reducers that we want to store
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const middlewares = [];
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+
+export const persistor = persistStore(store);
 
 export default store;
